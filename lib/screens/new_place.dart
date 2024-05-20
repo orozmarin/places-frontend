a436a165801897c781e1ac29898d5ed3776d6ae7
bytemerin <marin.oroz@byteink.hr>
2024-05-20 16:56:38 +0200

    Added validation to new Place input form. Starting to work on Router.

--- lib/screens/new_place.dart
+++ lib/screens/new_place.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:gastrorate/models/place.dart';
 import 'package:gastrorate/models/rating.dart';
 import 'package:gastrorate/theme/my_colors.dart';
+import 'package:gastrorate/tools/toast_helper_web.dart';
 import 'package:gastrorate/widgets/default_button.dart';
 import 'package:gastrorate/widgets/horizontal_spacer.dart';
 import 'package:gastrorate/widgets/input_field.dart';
 import 'package:gastrorate/widgets/page_body_card.dart';
-import 'package:gastrorate/widgets/rating_summary_card.dart';
 import 'package:gastrorate/widgets/place_rating_dialog.dart';
+import 'package:gastrorate/widgets/rating_summary_card.dart';
 import 'package:gastrorate/widgets/vertical_spacer.dart';
 
 class NewPlace extends StatefulWidget {
@@ -21,6 +22,8 @@ class NewPlace extends StatefulWidget {
 }
 
 class _NewPlaceState extends State<NewPlace> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -38,126 +41,160 @@ class _NewPlaceState extends State<NewPlace> {
           child: PageBodyCard(
             child: Container(
               padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  InputField(
-                    labelText: "Name",
-                    hintText: "Enter name",
-                    initialValue: widget.place.name,
-                    onChanged: (value) {
-                      widget.place.name = value;
-                    },
-                    isSmallInputField: true,
-                  ),
-                  const VerticalSpacer(8),
-                  InputField(
-                    labelText: "Street and number",
-                    hintText: "Enter address",
-                    initialValue: widget.place.address,
-                    onChanged: (value) {
-                      widget.place.address = value;
-                    },
-                    isSmallInputField: true,
-                  ),
-                  const VerticalSpacer(8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InputField(
-                        width: screenWidth * 0.5,
-                        labelText: "City",
-                        hintText: "Enter city",
-                        initialValue: widget.place.city,
-                        onChanged: (value) {
-                          widget.place.city = value;
-                        },
-                        isSmallInputField: true,
-                      ),
-                      const HorizontalSpacer(20),
-                      InputField(
-                        width: screenWidth * 0.3,
-                        labelText: "Postal code",
-                        hintText: "Enter postal code",
-                        initialValue:
-                            widget.place.postalCode != null ? widget.place.postalCode.toString() : '',
-                        onChanged: (value) {
-                          widget.place.postalCode = int.parse(value ?? '');
-                        },
-                        isSmallInputField: true,
-                      ),
-                    ],
-                  ),
-                  const VerticalSpacer(8),
-                  InputField(
-                    labelText: "Country",
-                    hintText: "Country",
-                    initialValue: widget.place.country,
-                    onChanged: (value) {
-                      widget.place.country = value;
-                    },
-                    isSmallInputField: true,
-                  ),
-                  const VerticalSpacer(8),
-                  Text("Ratings", style: Theme.of(context).textTheme.headlineSmall),
-                  const VerticalSpacer(8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      widget.place.firstRating == null
-                          ? IconButton(
-                              icon: const Icon(
-                                size: 50,
-                                semanticLabel: "Add",
-                                Icons.add_circle,
-                                color: MyColors.primaryColor,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InputField(
+                      labelText: "Name",
+                      hintText: "Enter name",
+                      validatorFunction: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a name';
+                        }
+                        return null;
+                      },
+                      initialValue: widget.place.name,
+                      onChanged: (value) {
+                        widget.place.name = value;
+                      },
+                      isSmallInputField: true,
+                    ),
+                    const VerticalSpacer(8),
+                    InputField(
+                      labelText: "Street and number",
+                      hintText: "Enter address",
+                      validatorFunction: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an address';
+                        }
+                        return null;
+                      },
+                      initialValue: widget.place.address,
+                      onChanged: (value) {
+                        widget.place.address = value;
+                      },
+                      isSmallInputField: true,
+                    ),
+                    const VerticalSpacer(8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InputField(
+                          width: screenWidth * 0.5,
+                          labelText: "City",
+                          hintText: "Enter city",
+                          validatorFunction: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a city';
+                            }
+                            return null;
+                          },
+                          initialValue: widget.place.city,
+                          onChanged: (value) {
+                            widget.place.city = value;
+                          },
+                          isSmallInputField: true,
+                        ),
+                        const HorizontalSpacer(20),
+                        InputField(
+                          width: screenWidth * 0.3,
+                          labelText: "Postal code",
+                          hintText: "Enter postal code",
+                          validatorFunction: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a postal code';
+                            }
+                            if (int.tryParse(value) == null) {
+                              return 'Please enter a valid number';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.number,
+                          initialValue: widget.place.postalCode != null ? widget.place.postalCode.toString() : '',
+                          onChanged: (value) {
+                            widget.place.postalCode = int.parse(value ?? '');
+                          },
+                          isSmallInputField: true,
+                        ),
+                      ],
+                    ),
+                    const VerticalSpacer(8),
+                    InputField(
+                      labelText: "Country",
+                      hintText: "Country",
+                      initialValue: widget.place.country,
+                      validatorFunction: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a country';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        widget.place.country = value;
+                      },
+                      isSmallInputField: true,
+                    ),
+                    const VerticalSpacer(8),
+                    Text("Ratings", style: Theme.of(context).textTheme.headlineSmall),
+                    const VerticalSpacer(8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        widget.place.firstRating == null
+                            ? IconButton(
+                                icon: const Icon(
+                                  size: 50,
+                                  semanticLabel: "Add",
+                                  Icons.add_circle,
+                                  color: MyColors.primaryColor,
+                                ),
+                                onPressed: () {
+                                  widget.place.firstRating ??= Rating(ambientRating: 1, foodRating: 1, priceRating: 1);
+                                  showRatingDialog(widget.place.firstRating!);
+                                },
+                              )
+                            : RatingSummaryCard(
+                                rating: widget.place.firstRating!,
+                                onEditRating: () {
+                                  showRatingDialog(widget.place.firstRating!);
+                                },
+                                onDeleteRating: () {
+                                  widget.place.firstRating = null;
+                                  setState(() {});
+                                },
                               ),
-                              onPressed: () {
-                                widget.place.firstRating ??=
-                                    Rating(ambientRating: 1, foodRating: 1, priceRating: 1);
-                                showRatingDialog(widget.place.firstRating!);
-                              },
-                            )
-                          : RatingSummaryCard(
-                              rating: widget.place.firstRating!,
-                              onEditRating: () {
-                                showRatingDialog(widget.place.firstRating!);
-                              },
-                              onDeleteRating: () {
-                                widget.place.firstRating = null;
-                                setState(() {});
-                              },
-                            ),
-                      const HorizontalSpacer(8),
-                      widget.place.secondRating == null
-                          ? IconButton(
-                              icon: const Icon(
-                                size: 50,
-                                semanticLabel: "Add",
-                                Icons.add_circle,
-                                color: MyColors.primaryColor,
+                        const HorizontalSpacer(8),
+                        widget.place.secondRating == null
+                            ? IconButton(
+                                icon: const Icon(
+                                  size: 50,
+                                  semanticLabel: "Add",
+                                  Icons.add_circle,
+                                  color: MyColors.primaryColor,
+                                ),
+                                onPressed: () {
+                                  widget.place.secondRating ??= Rating(ambientRating: 1, foodRating: 1, priceRating: 1);
+                                  showRatingDialog(widget.place.secondRating!);
+                                },
+                              )
+                            : RatingSummaryCard(
+                                rating: widget.place.secondRating!,
+                                onEditRating: () {
+                                  showRatingDialog(widget.place.secondRating!);
+                                },
+                                onDeleteRating: () {
+                                  widget.place.secondRating = null;
+                                  setState(() {});
+                                },
                               ),
-                              onPressed: () {
-                                widget.place.secondRating ??=
-                                    Rating(ambientRating: 1, foodRating: 1, priceRating: 1);
-                                showRatingDialog(widget.place.secondRating!);
-                              },
-                            )
-                          : RatingSummaryCard(
-                              rating: widget.place.secondRating!,
-                              onEditRating: () {
-                                showRatingDialog(widget.place.secondRating!);
-                              },
-                              onDeleteRating: () {
-                                widget.place.secondRating = null;
-                                setState(() {});
-                              },
-                            ),
-                    ],
-                  ),
-                  //TODO add photo upload
-                ],
+                      ],
+                    ),
+                    //TODO add photo upload
+                  ],
+                ),
               ),
             ),
           ),
@@ -168,8 +205,14 @@ class _NewPlaceState extends State<NewPlace> {
         child: ButtonComponent(
           text: "Save",
           onPressed: () {
-            widget.onSavePlace(widget.place);
-            Navigator.pop(context);
+            if (widget.place.firstRating == null || widget.place.secondRating == null)
+              toastHelperWeb.showToastError(context, "Please add ratings of your place");
+            else if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              widget.onSavePlace(widget.place);
+              toastHelperWeb.showToastSuccess(context, "Place saved!");
+              Navigator.pop(context);
+            }
           },
         ),
       ),
---
+++ lib/screens/settings.dart
@@ -0,0 +1,55 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:gastrorate/models/place.dart';
+import 'package:gastrorate/screens/home.dart';
+import 'package:gastrorate/store/app_state.dart';
+import 'package:gastrorate/store/places/places_actions.dart';
+
+class PlacesPage extends StatelessWidget {
+  const PlacesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, ViewModel>(
+      vm: () => Factory(this),
+      onInit: (Store<AppState> store) => store.dispatch(FetchPlacesAction()),
+      builder: (BuildContext context, ViewModel vm) => Home(
+        places: vm.places,
+        onFindAllPlaces: vm.onFindAllPlaces,
+        onDeletePlace: vm.onDeletePlace,
+      ),
+    );
+  }
+}
+
+class Factory extends VmFactory<AppState, PlacesPage, ViewModel> {
+  Factory(PlacesPage widget) : super(widget);
+
+  @override
+  ViewModel? fromStore() => ViewModel(
+    places: state.placesState.places,
+    onFindAllPlaces: () => dispatch(FetchPlacesAction()),
+    onDeletePlace: (place) => dispatch(DeletePlaceAction(place)),
+  );
+}
+
+class ViewModel extends Vm {
+  final List<Place>? places;
+  final Function() onFindAllPlaces;
+  final Function(Place place) onDeletePlace;
+
+  ViewModel({required this.places, required this.onFindAllPlaces, required this.onDeletePlace});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          super == other &&
+              other is ViewModel &&
+              runtimeType == other.runtimeType &&
+              places == other.places &&
+              onFindAllPlaces == other.onFindAllPlaces &&
+              onDeletePlace == other.onDeletePlace;
+
+  @override
+  int get hashCode => super.hashCode ^ places.hashCode ^ onFindAllPlaces.hashCode ^ onDeletePlace.hashCode;
+}
