20a9373bed6dc42b2c468e6c4e3fae7052f07622
orozmarin <marinoroz42@gmail.com>
2025-04-01 15:51:05 +0200

    New design for place_card.dart, added a swiper to home screen.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,7 +6,10 @@ import 'package:gastrorate/models/place.dart';
 import 'package:gastrorate/theme/my_colors.dart';
 import 'package:gastrorate/theme/theme_helper.dart';
 import 'package:gastrorate/widgets/custom_text.dart';
+import 'package:gastrorate/widgets/horizontal_line.dart';
 import 'package:gastrorate/widgets/place_card.dart';
+import 'package:gastrorate/widgets/place_card_swiper.dart';
+import 'package:gastrorate/widgets/vertical_spacer.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
@@ -36,6 +39,26 @@ class _HomeState extends State<Home> {
   Place? selectedPlace;
 
   bool _mapsInitialized = false;
+
+  final ScrollController _scrollController = ScrollController();
+  bool _isVisible = true;
+  double _previousScrollOffset = 0;
+
+  // Detect scroll direction and show or hide the button
+  void _onScroll() {
+    if (_scrollController.offset > _previousScrollOffset && _isVisible) {
+      // Scrolling down, hide the button
+      setState(() {
+        _isVisible = false;
+      });
+    } else if (_scrollController.offset < _previousScrollOffset && !_isVisible) {
+      // Scrolling up, show the button
+      setState(() {
+        _isVisible = true;
+      });
+    }
+    _previousScrollOffset = _scrollController.offset;
+  }
 
   void initRenderer() {
     if (_mapsInitialized) return;
@@ -68,73 +91,125 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const CustomText("Home", style: TextStyle(color: MyColors.navbarItemColor)),
         backgroundColor: MyColors.appbarColor,
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        controller: _scrollController,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.all(6),
+              padding: const EdgeInsets.only(left: 18, top: 8, bottom: 8),
               child: CustomText(
-                "Recently added",
-                style: Theme.of(context).textTheme.headlineSmall,
+                "Places near you",
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  Place place = widget.places![index];
-                  return PlaceCard(
-                    place: place,
-                    onDeletePlace: widget.onDeletePlace,
-                    onInitPlaceForm: widget.onInitPlaceForm,
-                  );
-                },
-                itemCount: widget.places!.length,
+            if (widget.places != null && widget.places!.isNotEmpty)
+              PlaceCardSwiper(
+                  places: widget.places!, onDeletePlace: widget.onDeletePlace, onInitPlaceForm: widget.onInitPlaceForm),
+            const VerticalSpacer(10),
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 22), child: HorizontalLine()),
+            const VerticalSpacer(6),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: CustomText(
+                "Recently added",
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
               ),
+            ),
+            ListView.builder(
+              padding: const EdgeInsets.only(bottom: 20),
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                Place place = widget.places![index];
+                return PlaceCard(
+                  place: place,
+                  onDeletePlace: widget.onDeletePlace,
+                  onInitPlaceForm: widget.onInitPlaceForm,
+                );
+              },
+              itemCount: widget.places!.length,
             ),
           ],
         ),
       ),
-      floatingActionButton: IconButton(
-          icon: const Icon(
-            Icons.add_circle,
-            color: MyColors.primaryDarkColor,
-          ),
-          iconSize: 50,
-          onPressed: () {
-            initRenderer();
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return PlacePicker(
-                    resizeToAvoidBottomInset: false,
-                    apiKey: Platform.isAndroid ? dotenv.env['MAPS_API'].toString() : dotenv.env['MAPS_API'].toString(),
-                    hintText: "Find a place ...",
-                    searchingText: "Please wait ...",
-                    selectText: "Select place",
-                    initialPosition: initialPosition,
-                    useCurrentLocation: true,
-                    selectInitialPosition: true,
-                    usePlaceDetailSearch: true,
-                    zoomControlsEnabled: true,
-                    onPlacePicked: (PickResult result) {
-                      setState(() {
-                        selectedPlace = Place.fromPickResult(result);
-                        widget.onInitPlaceForm(selectedPlace ?? Place());
-                      });
-                    },
-                  );
+      floatingActionButton: _isVisible
+          ? AnimatedOpacity(
+              opacity: 1.0, duration: const Duration(milliseconds: 300), child: buildAddPlaceButton(context))
+          : const AnimatedOpacity(
+              opacity: 0.0,
+              duration: Duration(milliseconds: 200),
+              child: SizedBox.shrink(),
+            ),
+    );
+  }
+
+  FloatingActionButton buildAddPlaceButton(BuildContext context) {
+    return FloatingActionButton.extended(
+      icon: const Icon(Icons.add, color: MyColors.navbarItemColor),
+      label: const CustomText(
+        "Add Place",
+        style: TextStyle(color: MyColors.navbarItemColor),
+      ),
+      backgroundColor: MyColors.primaryDarkColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        side: const BorderSide(
+          color: Colors.black12,
+          width: 1,
+        ),
+      ),
+      onPressed: () {
+        initRenderer();
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return PlacePicker(
+                resizeToAvoidBottomInset: false,
+                apiKey: Platform.isAndroid ? dotenv.env['MAPS_API'].toString() : dotenv.env['MAPS_API'].toString(),
+                hintText: "Find a place ...",
+                searchingText: "Please wait ...",
+                selectText: "Select place",
+                initialPosition: initialPosition,
+                useCurrentLocation: true,
+                selectInitialPosition: true,
+                usePlaceDetailSearch: true,
+                zoomControlsEnabled: true,
+                onPlacePicked: (PickResult result) {
+                  setState(() {
+                    selectedPlace = Place.fromPickResult(result);
+                    widget.onInitPlaceForm(selectedPlace ?? Place());
+                  });
                 },
-              ),
-            );
-          }),
+              );
+            },
+          ),
+        );
+      },
+      elevation: 6.0,
+      focusElevation: 10.0,
+      highlightElevation: 8.0,
+      splashColor: Colors.white.withOpacity(0.3),
     );
   }
 }
